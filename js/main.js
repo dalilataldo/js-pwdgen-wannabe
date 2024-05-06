c2e2bb642406f1e11834f35708b717f875d68f8a
dalilataldo <dalila.taldo@gmail.com>
2024-05-06 22:46:48 +0200

    add: parseInt

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const firstName = prompt('Inserisci il tuo nome');
 const lastName = prompt('Inserisci il tuo cognome');
 const color = prompt('Inserisci il tuo colore preferito');
+const numberone = prompt('Inserisci un numero');
+const numbertwo = prompt('Inserisci un altro numero');
 
-const password = firstName + '' + lastName + '' + color + '' + 21
-
+const password = firstName + lastName + color + (parseInt(numberone / numbertwo));
 
 console.log(password);
 document.getElementById('my_id').innerHTML = password;
